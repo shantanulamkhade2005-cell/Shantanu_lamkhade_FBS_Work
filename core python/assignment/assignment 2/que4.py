27af58b359e86bb9f0002f47b77cc5769b9beb4b
shantanulamkhade2005-cell <shantanulamkhade2005@gmail.com>
2026-09-04 16:13:00 +0530

    Add files via upload

---
+++ core python/assignment/assignment 2/que4.py
@@ -0,0 +1,10 @@
+print('area of triangel')
+base=float(input('enter the base:'))
+height=float(input('enter the height:'))
+area=0.5*base*height
+print('area of triangle is:',area)
+print('area of recatngle')
+length=float(input('enter the length:'))
+width=float(input('enter the width:'))
+r_area=length*width
+print(f'area of rectangle is:{r_area}')
