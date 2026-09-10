283a14b470d5638e777876c9f8bae00d605bd943
shantanulamkhade2005-cell <shantanulamkhade2005@gmail.com>
2026-09-10 16:12:06 +0530

    Refactor wall painting cost calculation

--- core python/test2/que4.py
+++ core python/test2/que4.py
@@ -1,6 +1,10 @@
-cost=int(input('enter the cost of single wall'))
-if(cost*4):
-    cost*=4
-    print('cost for painting the wall :',cost)
-else:
-    print()
+
+length = int(input('Enter length of wall: '))
+height = int(input('Enter height of wall: '))
+cost = int(input('Enter painting cost per sq.unit: '))
+
+area = 4 * length * height
+total_cost = area * cost
+
+print('Area of four walls =', area)
+print('Total cost of painting =', total_cost)
